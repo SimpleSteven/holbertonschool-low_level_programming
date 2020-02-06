40dfb70babac925f40ddf4ec0432080133fe5810
SimpleSteven <1649@holberton.com>
2020-02-06 22:46:00 +0000

    10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+int a;
+for (i = 0; i < 10; i++)
+{
+for (a = 0; a < 10; a++)
+{
+putchar(i + '0');
+putchar(a + '0');
+if (i != 9 || a != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+a = 0;
+}
+putchar('\n');
+return (0);
+}
