5ffc8cf09297df5d42f2f560c57d0cf48abb65b8
SimpleSteven <1649@holberton.com>
2020-02-06 21:58:38 +0000

    8_merge_problem

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,33 +11,10 @@
 int main(void)
 {
 int a;
-<<<<<<< HEAD
 char b;
 for (a = 0; a < 10; a++)
 putchar(a + '0');
 for (b = 'a'; b < 'g'; ++b)
 putchar(b);
 putchar('\n');
-=======
-for (a = 0; a < 16; a++)
-{
-long int q = a, rmd, dn = 0, m, l;
-int i = 1, j, tmp;
-char s;
-for (l = q; l > 0; l = l / 16)
-{
-tmp = l % 16;
-if (tmp < 10)
-tmp = tmp + 48; else
-tmp = tmp + 55;
-dn = dn * 100 + tmp;
-}
-for (m = dn; m > 0; m = m / 100)
-{
-s = m % 100;
-putchar (tolower(s));
-}
-}
-putchar ('\n');
->>>>>>> 481284446ec0fb61ee0aac2e7266ccfc680145fe
 }
