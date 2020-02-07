0b7d3cedf54784bcd19044450f7b45219a250026
SimpleSteven <60364291+SimpleSteven@users.noreply.github.com>
2020-02-06 22:51:11 -0500

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - entry point
+*
+* Description:  prints all possible different combinations of three digits.
+*
+* Return: 0
+*/
 
 int main(void)
 {
-int d1, d2, d3, d4;
-for (d1 = '0'; d1 <= '9'; d1++)
-for (d2 = '0'; d2 <= '9'; d2++)
-for (d3 = '0'; d3 <= '9'; d3++)
-for (d4 = '0'; d4 <= '9'; d4++)
-{
-if (d1 < d3 || (d1 == d3 && d2 < d4))
-{
-putchar(d1);
-putchar(d2);
-putchar(' ');
-putchar(d3);
-putchar(d4);
-if (d1 != '9' || d2 != '8' ||
-d3 != '9' || d4 != '9')
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	int u, t;
+
+	for (u = 0; u <= 99; u++)
+	{
+		for (t = 0; t <= 99; t++)
+		{
+			if  (u < t)
+			{
+				putchar(u / 10 + '0');
+				putchar(u % 10 + '0');
+				putchar(32);
+				putchar(t / 10 + '0');
+				putchar(t % 10 + '0');
+				if (u != 98)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
