d96677e3acc78f21894981b7e4664858a7174f1e
SimpleSteven <adduser@DESKTOP-6548V1I.localdomain>
2020-02-11 21:45:03 -0500

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - show the last digit of a number.
+ * @n: first int.
+ * @i: second int.
+ * Return: return the add.
+ */
+
+int add(int n, int i)
+{
+	int a;
+a = n + i;
+return(a);
+}
