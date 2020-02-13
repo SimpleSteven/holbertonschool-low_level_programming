bf129426a14aa800d0f0ecf8bcd77fb73a6406bc
SimpleSteven <1649@holbertonschool.com>
2020-02-13 17:44:41 -0500

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * print_diagonal - writes the character c to stdout
+ * @n: int
+ */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	for (a = 0; a < n ; a++)
+	{
+		for (b = 0; b < a; b++)
+		_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
