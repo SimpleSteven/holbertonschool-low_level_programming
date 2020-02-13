6802aa9f15eff2f81080efa2f54078225c2a72e6
SimpleSteven <1649@holbertonschool.com>
2020-02-13 16:01:56 -0500

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * _isdigit - writes the character c to stdout
+ * @c: the char
+ * Return: return 1 if is uppercase and 0 if isnt
+ */
+
+int _isdigit(int c)
+{
+	if (c > 47  && c < 57)
+		c = 1;
+	else
+	c = 0;
+	return (c);
+}
