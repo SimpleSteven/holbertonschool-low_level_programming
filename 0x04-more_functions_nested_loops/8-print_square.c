345f0e7e78aa6282ea29ad765e3abcc6e28bc7fb
SimpleSteven <1649@holbertonschool.com>
2020-02-13 17:56:04 -0500

    else_fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,6 +10,7 @@ void print_square(int size)
 {
 	int a, b;
 
+	if (size > 0)
 	for (a = 0; a < size; a++)
 	{
 		for (b = 0; b < size; b++)
@@ -19,4 +20,6 @@ void print_square(int size)
 
 		_putchar('\n');
 	}
+	else
+	_putchar('\n');
 }
