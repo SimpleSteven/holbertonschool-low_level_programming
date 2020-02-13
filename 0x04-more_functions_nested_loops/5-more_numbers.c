4c60bfc05bf2b54efe8a9345c908d4945c728c7e
SimpleSteven <1649@holbertonschool.com>
2020-02-13 16:29:45 -0500

    e

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * more_numbers - writes the character c to stdout
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= ''; ++a)
+	_putchar(a);
+	_putchar('\n');
+}
