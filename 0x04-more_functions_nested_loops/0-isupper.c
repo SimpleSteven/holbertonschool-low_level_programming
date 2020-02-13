f43194d7c4450690936d6ae7aae8567789a8c92a
SimpleSteven <1649@holbertonschool.com>
2020-02-13 15:56:04 -0500

    library_fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,13 +4,13 @@
 /**
  * _isupper - writes the character c to stdout
  * @c: the char
+ * Return: return 1 if is uppercase and 0 if isnt
  */
 
 int _isupper(int c)
 {
-	int c;
 	if (c > 64 && c < 91)
-	c = 1;
+		c = 1;
 	else
 	c = 0;
 	return (c);
