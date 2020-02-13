acffbf6cc891728e8e1b63b3c823216bc1f9fb0f
SimpleSteven <1649@holbertonschool.com>
2020-02-13 17:07:32 -0500

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,24 @@
 
 void more_numbers(void)
 {
-	int a, b;
-
-	for (a = '0'; a <= ''; ++a)
-	_putchar(a);
-	_putchar('\n');
+	int a, b, c;
+	
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 2; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				if (b == 1)
+				_putchar(b + '0');
+				_putchar(c + '0');
+				if (b == 1 && c == 4)
+				{
+					b = 3;
+					c = 10;
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
