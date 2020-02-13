77e6e92e97a338081fb204e2f9481724df2cac36
SimpleSteven <1649@holbertonschool.com>
2020-02-13 16:07:01 -0500

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * mul - writes the character c to stdout
+ * @a: first int
+ * @b: second int
+ * Return: return 1 if is uppercase and 0 if isnt
+ */
+
+int mul(int a, int b)
+{
+	a = a * b;
+	return (a);
+}
