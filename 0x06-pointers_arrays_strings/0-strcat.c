aaacbd2a4e56d588bf6d3dfb268773d4868baaea
SimpleSteven <1649@holbertonschool.com>
2020-02-20 18:17:35 -0500

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - check the code for Holberton School students.
+ * @dest: the char
+ * @src: the other char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a, b, c;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+	}
+	for (b = 0; src[b] != '\0'; b++)
+	{
+	}
+	for (c = 0; c < b; c++)
+	{
+		dest[a] = src[c];
+		a++;
+	}
+
+	return dest;
+}
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+int _putchar(char n);
+
+char *_strcat(char *dest, char *src);
+
+#endif /* HOLBERTON_H */
