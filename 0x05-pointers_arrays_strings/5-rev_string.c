5bad005b544624aa15b1c0edd7e0b717382682df
SimpleSteven <1649@holbertonschool.com>
2020-02-20 15:51:20 -0500

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * rev_string- check the code for Holberton School students.
+ * @s: the char
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i, j = 0;
+	char a[10];
+
+	for (i = 0; s[i] != '\0'; i++)
+		a[i] = s[i];
+	for (i--; i >= 0; i--)
+	{
+		s[i] = a[j];
+		j++;
+	}
+}
