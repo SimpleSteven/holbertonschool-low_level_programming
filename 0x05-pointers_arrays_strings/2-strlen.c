e22376d91d834c481cb673bb990438cee67ece98
SimpleSteven <1649@holbertonschool.com>
2020-02-20 14:50:01 -0500

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - check the code for Holberton School students
+ * @s: intb
+ * Return: the length of the parameter
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' ;i++)
+	{
+	}
+	return (i);
+}
