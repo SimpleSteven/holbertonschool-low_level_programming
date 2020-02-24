c247a79c0bf50b79d5be04736815216c62a9c63a
SimpleSteven <1648@holbertonschool.com>
2020-02-24 13:40:06 -0500

    inclusive_null

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,12 @@
 char *_strchr(char *s, char c)
 {
 
-	while (s[0] != '\0')
+	while (s[0] != '\0' && s[0] != c)
 	{
-		if (s[0] == c)
-			return (s);
 		s++;
 	}
+		if (s[0] == c)
+			return (s);
+	
 	return (0);
 }
