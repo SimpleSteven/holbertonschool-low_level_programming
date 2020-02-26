0f32beefc668467e4c1abc7747a86d76855566ba
SimpleSteven <1648@holbertonschool.com>
2020-02-25 19:05:38 -0500

    8

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - check the code for Holberton School students.
+ * @s: the char
+ * @to: the pointer
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
