d871fbf1b18716b41b63e19be4c52e204115a80b
SimpleSteven <1648@holbertonschool.com>
2020-02-26 10:54:01 -0500

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - check the code for Holberton School students.
+ * @s: the char
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+		_putchar(*s);
+}
