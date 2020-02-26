71133fad66d6b7f7408694d5f1036deb7e1c8aa9
SimpleSteven <1648@holbertonschool.com>
2020-02-26 15:55:57 -0500

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - check the code for Holberton School students.
+ * @n: the int
+  *@cnt: the counter
+ * Return: the char concatenated
+ */
+
+int square_root(int cnt, int n);
+
+int _sqrt_recursion(int n)
+{
+	int cnt = 1;
+
+	return (square_root(cnt, n));
+}
+
+/**
+  * square_root - the other funtcion
+  * @cnt: the counter
+  * @n: the root
+  * Return: the int
+ */
+
+int square_root(int cnt, int n)
+{
+	if (n < 0 || cnt * cnt > n)
+	{
+		return (-1);
+	}
+	else if (cnt * cnt == n)
+	{
+		return (cnt);
+	}
+	else if (n > 1)
+	{
+		return (square_root(cnt + 1, n));
+	}
+	return (0);
+}
