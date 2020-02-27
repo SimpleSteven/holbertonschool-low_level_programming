664b44de6558ed2bc24c9d49116bb5859b299607
SimpleSteven <1648@holbertonschool.com>
2020-02-26 19:00:29 -0500

    100

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * wildcmp- check the code for Holberton School students.
+ * @s1: the char
+ * @s2: the other char
+ * Return: the char concatenated
+ */
+
+int wildcmp(char *s1, char s2)
+{
+	
+}
