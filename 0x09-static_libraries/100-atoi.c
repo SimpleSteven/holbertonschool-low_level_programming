d61889fde28f22c6eb17711675b90f278030c883
SimpleSteven <1648@holbertonschool.com>
2020-03-01 23:40:45 -0500

    atoi_code_added

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,5 +8,33 @@
 
 int _atoi(char *s)
 {
-	return (*s);
+	int i, crrtval, sign;
+	unsigned int rta;
+
+	i = 0;
+	rta = 0;
+	sign = 1;
+	while (s[i] != '\0')
+	{
+		crrtval = s[i];
+		if (crrtval >= 48 && crrtval <= 57)
+		{
+			if (rta > 0)
+			{
+				rta = rta * 10 + crrtval - 48;
+			}
+			else
+				rta = crrtval - 48;
+		}
+		else if (crrtval == 45)
+		{
+			sign *= -1;
+		}
+		else if (crrtval > 0 && rta > 0)
+		{
+			break;
+		}
+		i++;
+	}
+	return (rta * sign);
 }
