19a4f3bc6d2cac6fb1bf9c579ea523ef2ec63170
SimpleSteven <1648@holbertonschool.com>
2020-03-02 14:38:24 -0500

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - the function
+  * @argc: the int
+  * @argv: the char
+  * Return: 0 if is succesful
+  */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
