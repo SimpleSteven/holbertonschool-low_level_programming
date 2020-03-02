f52ec78f68bca90cf7a33707e6fc85c0e8a560c8
SimpleSteven <1648@holbertonschool.com>
2020-03-02 18:35:26 -0500

    100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - the function
+  * @argc: the int
+  * @argv: the char
+  * Return: the int
+  */
+
+int main(int argc, char *argv[])
+{
+	int i, j = 0, value = atoi(argv[1]);
+	int cents[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (value < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (value > cents[i])
+			{
+				j += value / cents[i];
+				value %= cents[i];
+			}
+		}
+	}
+	printf("%i\n", j);
+
+	return (0);
+}
