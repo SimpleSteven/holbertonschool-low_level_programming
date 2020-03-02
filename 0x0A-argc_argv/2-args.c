d47a04cfc8d377a5f5458755cefa488996edfd50
SimpleSteven <1648@holbertonschool.com>
2020-03-02 14:53:46 -0500

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - the funtcion
+  * @argc: the int
+  * @argv: the char
+  * Return: the int
+  */
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	printf("%s,\n", argv[i]);
+}
