f13db0f9f33cadb4c7f868aab64c77a334e2cf01
SimpleSteven <1648@holbertonschool.com>
2020-03-02 17:37:35 -0500

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - the function
+  * @argc: the int
+  * @argv: the char
+  * Return: the int
+  */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+	
+	if (argc < 3)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	sum += atoi(argv[i]);
+	}
+	printf("%i\n", sum);
+	return (0);
+}
