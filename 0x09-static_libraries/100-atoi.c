1ea1d1a68b143130a353c6d3010cc92931925feb
SimpleSteven <1648@holbertonschool.com>
2020-03-01 23:35:01 -0500

    compled_again_with_the_name_corrected

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,18 +8,5 @@
 
 int _atoi(char *s)
 {
-	float a;
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if ((s[i--] > 47 && s[i] < 58) && (s[i] < 48 && s[i] > 57))
-				break;
-		if (s[i] > 47 && s[i] < 58)
-		{
-			a = s[i];
-		}
-	}
-
-	return (a);
+	return (*s);
 }
