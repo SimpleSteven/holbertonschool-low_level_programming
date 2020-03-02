6a89016f6770a247f4a38d895d60f4bd7134fcd0
SimpleSteven <1648@holbertonschool.com>
2020-03-02 00:17:47 -0500

    is_alpha_protype_fixed

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * _islower - check lowercaser.
+ * _isalpha - check lowercaser.
  * @n: The int to print
  * Return: 1 if n is lowercase.
  */
 
-int _islower(int n)
+int _isalpha(int n)
 {
 if (n > 96 && n < 123)
 n = 1;
