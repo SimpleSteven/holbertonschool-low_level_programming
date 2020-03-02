379e070d2e78de47cfb80eefedfbb2d2c64dfbf4
SimpleSteven <1648@holbertonschool.com>
2020-03-02 13:37:42 -0500

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - the  main function
+ * @argc:  the int
+ * @argv: the char
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Project folder 10
