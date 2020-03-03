431229f4e87dd0f1f949d3f4f8d359c5f6e4688c
SimpleSteven <1648@holbertonschool.com>
2020-03-03 14:35:04 -0500

    if_there's_not_enough_space

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,10 +13,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *a;
 
-	if (size == 0 || c == 0)
+	if (size == 0)
 		return (NULL);
 
 	a = malloc(sizeof(char) * size);
+	if (a == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		a[i] = c;
