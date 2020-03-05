da0b7aff6cdc40cc886115330352f2bcc02bb839
SimpleSteven <1648@holbertonschool.com>
2020-03-05 14:48:46 -0500

    malloc_instead_calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,15 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *a;
-	
-	if (nmemb == 0)
+	char *a;
+	unsigned int count;
+
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	a = calloc(nmemb, size);
+	a = malloc(nmemb * size);
 	if (a == NULL)
 		return (NULL);
+	for (count = 0; count < nmemb * size; count++)
+		a[count] = '0';
 	return (a);
 }
