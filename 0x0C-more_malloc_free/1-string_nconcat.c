6c436373128f2fae92b93bfd90df2533b7d076a8
SimpleSteven <1648@holbertonschool.com>
2020-03-05 13:40:13 -0500

    minor_than_n_fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (; s1[count]; count++)
 	{
 	}
-	for(; s2[count2]; count2++)
+	for (; s2[count2] && count < n; count2++)
 	{
 	}
 	a = malloc(count + count2 + 1);
