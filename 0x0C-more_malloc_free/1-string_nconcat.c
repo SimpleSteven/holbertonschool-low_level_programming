dcd779cd2c80284ba15e32c18d4c6598a7cdf6eb
SimpleSteven <1648@holbertonschool.com>
2020-03-05 13:04:42 -0500

    1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+  * string_nconcat - the function
+  * @s1: the char
+  * @s2: the other char
+  * @n: the int
+  * Return: the char
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *a;
+	int count = 0, count2 = 0, con = 0;
+
+	while (con != 2)
+	{
+		if (s1[count] != '\0' && con == 0)
+			count++;
+		else
+			con = 1;
+		if (con == 1 && s2[count2] != '\0' && count2 < n)
+			count2++;
+		else if (con == 1)
+			con = 2;
+	}
+	a = malloc(count + count2 + 1);
+	if (a == NULL)
+		return (NULL);
+	con = 0;
+	count = 0;
+	count2 = 0;
+	while (con != 2)
+	{
+		if (s1[count] != '\0' && con == 0)
+		{
+			a[count] = s1[count];
+			count++;
+		}
+		else
+			con = 1;
+		if (con == 1 && s2[count2] != '\0' && count2 < n)
+		{
+			a[count] = s2[count2];
+			count++;
+			count2++;
+		}
+		else if (con == 1)
+			con = 2;
+	}
+	a[count] = '\0';
+	return (a);
+}
