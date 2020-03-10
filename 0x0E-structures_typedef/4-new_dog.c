e8a0e6d23dda78236175c5c4f1c47e9e7eb82903
SimpleSteven <1648@holbertonschool.com>
2020-03-09 20:39:05 -0500

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+char *_strdup(char *str);
+
+/**
+  * new_dog - the function
+  * @name: the char pointer
+  * @age: the int
+  * @owner: the other char pointer
+  * Return: the new struct
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *new_dog;
+	char *s1;
+	char *s2;
+
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	s1 = _strdup(new_dog->name);
+	s2 = _strdup(new_dog->owner);
+	free(s1);
+	free(s2);
+	return (new_dog);
+}
+
+/**
+  * _strdup - the function
+  * @str: the char
+  * Return: the char
+  */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *a;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	i++;
+	a = malloc(sizeof(char) * i);
+	if (a == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+	{
+		a[j] = str[j];
+	}
+
+	return (a);
+}
