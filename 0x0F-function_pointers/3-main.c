4b3d2335957a6e7e9811f000cf5cf63a651e8c21
SimpleSteven <1648@holbertonschool.com>
2020-03-11 16:57:47 -0500

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+  * main - the function
+  * @argc: the counter
+  * @argv: the vectors
+  * Return: the int
+  */
+
+int main(int argc, char *argv[])
+{
+	int i, a, b;
+
+	if (argc != 4)
+	{
+		printf("ERROR\n");
+		exit(98);
+	}
+	else if (argv[2][0] != '+' && argv[2][0] != '*' && argv[2][0] != '-'
+		&& argv[2][0] != '/' && argv[2][0] != '%')
+	{
+		printf("ERROR\n");
+		exit(99);
+	}
+	else if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[1]) == 0 || atoi(argv[3]) == 0))
+	{
+		printf("ERROR\n");
+		exit(100);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	i = get_op_func(argv[2])(a, b);
+	printf("%i\n", i);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "3-calc.h"
+
+/**
+  * op_add - returns the sum of a and b
+  * op_sub - return the difference of a and b
+  * @a: first int
+  * @b: second int
+  * Return: the operation
+  */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - the function
+  * @a: the int
+  * @b: the int
+  * Return: the operation
+  */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+  * op_mul - the function
+  * @a: the int
+  * @b: the int
+  * Return: the operation
+  */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - the function
+  * @a: the int
+  * @b: the int
+  * Return: the operation
+  */
+
+
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * op_mod - the function
+  * @a: the int
+  * @b: the int
+  * Return: the operation
+  */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
