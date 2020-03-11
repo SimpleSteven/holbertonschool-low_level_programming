cba9cf3c12450f0e3b46f95517e9542935a941e9
SimpleSteven <1648@holbertonschool.com>
2020-03-11 17:39:36 -0500

    if_third_argument_have_more_than_one_char

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,13 +18,14 @@ int main(int argc, char *argv[])
 		printf("ERROR\n");
 		exit(98);
 	}
-	else if (argv[2][0] != '+' && argv[2][0] != '*' && argv[2][0] != '-'
-		&& argv[2][0] != '/' && argv[2][0] != '%')
+	else if ((argv[2][0] != '+' && argv[2][0] != '*' && argv[2][0] != '-'
+		&& argv[2][0] != '/' && argv[2][0] != '%') || argv[2][1] != '\0')
 	{
 		printf("ERROR\n");
 		exit(99);
 	}
-	else if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[1]) == 0 || atoi(argv[3]) == 0))
+	else if ((argv[2][0] == '/' ||
+		argv[2][0] == '%') && (atoi(argv[1]) == 0 || atoi(argv[3]) == 0))
 	{
 		printf("ERROR\n");
 		exit(100);
