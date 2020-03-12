383b497ef92eb6c22052b3179b079d66bac738f2
SimpleSteven <60364291+SimpleSteven@users.noreply.github.com>
2020-03-11 22:13:38 -0500

    02xxH

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -33,8 +33,8 @@ int main(int argc, char *argv[])
 	temp = (unsigned char *)&main;
 	for (count = 0; count < atoi(argv[1]) - 1; count++)
 	{
-		printf("%x ", temp[count]);
+		printf("%02hhX ", temp[count]);
 	}
-	printf("%x\n", temp[count]);
+	printf("%02hhX\n", temp[count]);
 	return (0);
 }
