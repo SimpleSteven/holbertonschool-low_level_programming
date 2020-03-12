855adb6dbccad4344a91e8fd1b10b71c6e2fe346
SimpleSteven <1648@holbertonschool.com>
2020-03-11 23:05:30 -0500

    02hhx_for_hexadecimal_lowercase

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 	temp = (unsigned char *)&main;
 	for (count = 0; count < atoi(argv[1]) - 1; count++)
 	{
-		printf("%02hhX ", temp[count]);
+		printf("%02hhx ", temp[count]);
 	}
 	printf("%02hhx\n", temp[count]);
 	return (0);
