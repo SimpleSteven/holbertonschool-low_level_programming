43e585688b5c26a5726280ec7a7b9bfbf56b8677
SimpleSteven <1648@holbertonschool.com>
2020-03-11 20:18:02 -0500

    4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,12 +15,12 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(1);
 	}
 	if (atoi(argv[1]) < 1)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(2);
 	}
 	temp = (unsigned char *)&main;
