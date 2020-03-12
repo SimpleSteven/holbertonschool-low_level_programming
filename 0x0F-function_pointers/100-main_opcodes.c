96b47058bba758692f9dc313ccd09de3d6f78820
SimpleSteven <1648@holbertonschool.com>
2020-03-11 20:44:20 -0500

    4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	unsigned char *temp;
-	int count;
+	int count, i;
 
 	if (argc != 2)
 	{
@@ -23,12 +23,18 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(2);
 	}
+	for (i = 0; i < argv[1][i] != '\0'; i++)
+	{
+		if (argv[1][i] > 47 && argv[1][i] < 58)
+			continue;
+		printf("Error\n");
+		exit(1);
+	}
 	temp = (unsigned char *)&main;
 	for (count = 0; count < atoi(argv[1]) - 1; count++)
 	{
 		printf("%x ", temp[count]);
 	}
 	printf("%x\n", temp[count]);
-
 	return (0);
 }
