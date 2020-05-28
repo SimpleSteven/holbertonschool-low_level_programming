af385ae5c558928add781d9c6c5b5c36676bbc07
SimpleSteven <1648@holbertonschool.com>
2020-05-28 18:30:48 -0500

    Function name fixed and iteration of the linked list added.

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,11 +6,14 @@
   * Return: the numbers of nodes in the linked list in size_t
   **/
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t counter = 0;
 
 	while (h != NULL)
+	{
 		counter++;
+		h = h->next;
+	}
 	return (counter);
 }
