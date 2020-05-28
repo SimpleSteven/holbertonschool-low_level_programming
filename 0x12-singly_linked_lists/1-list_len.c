b0fc5f99bba04fd929f507d056fac6a0fe2d07c5
SimpleSteven <1648@holbertonschool.com>
2020-05-28 13:18:22 -0500

    Iterate the node in the loop added.

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-  * list_len - count the nodes of a linked list
+  * list_len - count all the the nodes in a linked list
   * @h: the head node of the linked list
   * Return: the numbers of nodes in the linked list in size_t
   **/
@@ -11,6 +11,9 @@ size_t list_len(const list_t *h)
 	size_t counter = 0;
 
 	while (h != NULL)
+	{
 		counter++;
+		h = h->next;
+	}
 	return (counter);
 }
