271b8668e4d435001e0050820fc7a92e2cf8952b
SimpleSteven <1648@holbertonschool.com>
2020-05-28 19:02:04 -0500

    Free a linked list and the the head node to null.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+  * free_listint - free a linked list
+  * @head: the head node of the linked list
+  **/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current_node = *head;
+	listint_t *prev_node = *head;
+
+	if (*head == NULL)
+		return;
+	while (current_node != NULL)
+	{
+		current_node = current_node->next;
+		free(prev_node);
+		prev_node = current_node;
+	}
+	*head = NULL;
+}
