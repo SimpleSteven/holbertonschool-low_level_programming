f7c97addcb7107fb305ac941fb2ab3cf3cef8b71
SimpleSteven <1648@holbertonschool.com>
2020-05-28 23:06:10 -0500

    Insert a new node at a given position.

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+  * insert_nodeint_at_index - sum all the values of the nodes of a linked list
+  * @head: the first node of the linked list
+  * @idx: the given position
+  * @n: the value of the new node
+  * Return: the searched node
+  **/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current_node = (*head);
+	listint_t *new_node;
+	unsigned int count = 0;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = (*head);
+		(*head) = new_node;
+	}
+	while (current_node && count + 1 <= idx)
+	{
+		count++;
+		current_node = current_node->next;
+	}
+	new_node->next = current_node->next;
+	current_node->next = new_node;
+	return (current_node);
+}
