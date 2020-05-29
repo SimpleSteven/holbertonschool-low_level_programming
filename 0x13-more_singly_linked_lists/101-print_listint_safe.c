2239dd51a0dd2510d719a7f2d0d7e011204502bf
SimpleSteven <1648@holbertonschool.com>
2020-05-29 15:28:36 -0500

    Some minor chages.

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+  * print_listint_safe - print all the nodes in a linked list safe
+  * @head: the head node of the linked list
+  * Return: the numbers of nodes in the linked list in size_t
+  **/
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t counter = 0;
+	const listint_t *tmp_node = head;
+
+	if (!head)
+		exit (98);
+	while (tmp_node != NULL)
+	{
+		counter++;
+		printf("[%p] %i\n", (void *)tmp_node, tmp_node->n);
+		tmp_node = tmp_node->next;
+		if (!tmp_node)
+			return (counter);
+	}
+	return (counter);
+}
