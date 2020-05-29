857ee1281f2ad88f35db719549fe913c958cd083
SimpleSteven <1648@holbertonschool.com>
2020-05-29 01:12:43 -0500

    protoype change.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -49,6 +49,6 @@ size_t free_listint_safe(listint_t **h);
 
 listint_t *find_listint_loop(listint_t *head);
 
-listint_t *_rev(**head, listint_t *c_node, listint_t *p_node);
+int _rev(listint_t **head, listint_t *c_node, listint_t *p_node);
 
 #endif
