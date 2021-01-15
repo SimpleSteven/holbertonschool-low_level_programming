2e0e6333a846c4ffd4503841cc2b604eb4fc4563
SimpleSteven <1649@holbertonschool.com>
2021-01-15 14:23:50 -0500

    Betty errors fixed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,11 +10,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash = malloc(sizeof(hash_table_t));
+
 	if (new_hash == NULL)
-		return NULL;
-	new_hash->array = malloc(sizeof(hash_node_t*) * size);
+		return (NULL);
+	new_hash->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_hash->array == NULL)
-		return NULL;
+		return (NULL);
 	new_hash->size = size;
-	return new_hash;
+	return (new_hash);
 }
